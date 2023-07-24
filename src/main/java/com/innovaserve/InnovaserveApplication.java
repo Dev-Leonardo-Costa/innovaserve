7eaa24915a372f37742bb357531baa55c280b65d
Leonardo Costa <devleonardo27@gmail.com>
2023-07-24 20:52:13 -0300

    Inicio do projeto

---
+++ src/main/java/com/innovaserve/InnovaserveApplication.java
@@ -0,0 +1,13 @@
+package com.innovaserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innovaserve/InnovaserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovaserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
